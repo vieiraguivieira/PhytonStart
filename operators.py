0f64b8f694f3db998938ceb7367d83de93c3d5bd
Guilherme Santos <gv90.rocha28@gmail.com>
2020-08-06 15:46:28 +0100

    Class 28

--- operators.py
+++ operators.py
@@ -5,9 +5,22 @@ print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
-print(a // b) #integer division, rounded towards minus infinity
-print(a % b) #the raminder after integer division
+print(a // b) # integer division, rounded towards minus infinity
+print(a % b) # the raminder after integer division
 
 print()
 
 print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+print(((a + b) / 3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+
+print(a / (b * a) / b)
+
