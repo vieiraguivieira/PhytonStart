8fc2322ee1e47a320662d92ea2e3ddd3c109b76c
Guilherme Santos <gv90.rocha28@gmail.com>
2020-08-06 21:21:50 +0100

    Class 30

--- strings2.py
+++ strings2.py
@@ -1,28 +1,24 @@
+
+
 parrot = "Norwegian Blue"
 
-print(parrot)
+print(parrot[0:6])  # Norweg - the last caracther is not included
+print(parrot[3:5])
+print(parrot[0:9])
+print(parrot[:9])
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[10:14])
+print(parrot[10:])
 
-print()
+print(parrot[:6])
+print(parrot[6:])
 
-print(parrot[-11])
-print(parrot[-1])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+print(parrot[:6] + parrot[6:])
 
-print()
+print(parrot[:])
 
-print(parrot[3-14])
-print(parrot[4-14])
-print(parrot[9-14])
-print(parrot[3-14])
-print(parrot[6-14])
-print(parrot[8-14])
+letters = "abcdefghijklmnoprstuvwxyz"
+
+print(letters[6] + letters[-6] + letters[8] + letters[11] + letters[7] + letters[4] + letters[-9]
+      + letters[12] + letters[4])
+
