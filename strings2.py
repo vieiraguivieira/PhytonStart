dcdedf41d601914ec602806ccc07bab0ce68c81f
Guilherme Santos <gv90.rocha28@gmail.com>
2020-08-06 21:10:04 +0100

    Class 29

--- strings2.py
+++ strings2.py
@@ -1,6 +1,7 @@
 parrot = "Norwegian Blue"
 
 print(parrot)
+
 print(parrot[3])
 print(parrot[4])
 print(parrot[9])
@@ -8,5 +9,20 @@ print(parrot[3])
 print(parrot[6])
 print(parrot[8])
 
+print()
 
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
 
+print()
+
+print(parrot[3-14])
+print(parrot[4-14])
+print(parrot[9-14])
+print(parrot[3-14])
+print(parrot[6-14])
+print(parrot[8-14])
