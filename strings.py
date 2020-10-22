6b8320da0c5caf7cae387e5ff6b9f8b027193954
Guilherme Santos <gv90.rocha28@gmail.com>
2020-10-22 21:16:06 +0100

    Class 41

--- strings.py
+++ strings.py
@@ -25,6 +25,6 @@ age_in_words = "2 years"
 print(name + f" is {age} years old")
 print(type(age_in_words))
 
-print(f"Pi is approximately {22/7:<12.3f}")
+print(f"Pi is approximately {22/7:5.3f}")
 pi = 22/7
 print(f"Pi is approximately {pi:<12.3f}")
