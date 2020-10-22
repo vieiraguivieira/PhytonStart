0e2a0911a3bdfd8787854b8a47781cdf12072e34
Guilherme Santos <gv90.rocha28@gmail.com>
2020-10-22 21:59:21 +0100

    Class 44

--- blocksStatements.py
+++ blocksStatements.py
@@ -7,6 +7,7 @@ age = int(input("How old are you? {0}? ".format(name)))
 # else:
 #     print("You're not old enough to vote. You may vote in {0} years.".format(18 - age))
 
+#debugging
 if age < 18:
     print("You're not old enough to vote. You may vote in {0} years.".format(18 - age))
 elif age == 900:
