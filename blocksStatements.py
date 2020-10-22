d1d478478d14cae31b559a5c19221da31b597c92
Guilherme Santos <gv90.rocha28@gmail.com>
2020-10-22 21:46:44 +0100

    Class 43

--- blocksStatements.py
+++ blocksStatements.py
@@ -1,8 +1,17 @@
 name = input("Please enter your name: ")
 age = int(input("How old are you? {0}? ".format(name)))
 
-if age >= 18:
+# if age >= 18:
+#     print("You can vote!")
+#     print("Please put an X in the box.")
+# else:
+#     print("You're not old enough to vote. You may vote in {0} years.".format(18 - age))
+
+if age < 18:
+    print("You're not old enough to vote. You may vote in {0} years.".format(18 - age))
+elif age == 900:
+    print("You're lying!")
+    # elif é else if
+else:
     print("You can vote!")
     print("Please put an X in the box.")
-else:
-    print("You're not old enough to vote. You may vote in {0} years.".format(18 - age))
